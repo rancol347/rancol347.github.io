cfc1ceda225852b0b5e7c226be9790426d857267
rancol347 <romandriftovskiy@gmail.com>
2026-07-17 13:01:03 +0700

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Создаем контейнер для шапки
+    const header = document.createElement('div');
+    header.className = 'navbar';
+    header.innerHTML = `
+        <div class="logo" style="cursor: pointer;" onclick="window.location.href='/'">RANCOL<span>.DEV</span></div>
+        <div class="nav-links">
+            <a href="/">ГАРАЖ</a>
+            <a href="https://t.me" target="_blank">TELEGRAM</a>
+        </div>
+    `;
+    
+    // Вклеиваем шапку в самый верх тега body на любой странице
+    document.body.insertBefore(header, document.body.firstChild);
+});
